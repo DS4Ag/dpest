a5839fc224b2bac43916c847c8de9a1b9c389b45
luizvargaz <luizvargaz@hotmail.com>
2025-02-19 08:23:52 -0500

    Cleaned project structure

--- docs/conf.py
+++ docs/conf.py
@@ -30,10 +30,10 @@ autodoc_mock_imports = ["flopy", "pyemu"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
-# html_static_path = ['_static']
+html_theme = 'alabaster'
+html_static_path = ['_static']
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# import sphinx_rtd_theme
+# html_theme = "sphinx_rtd_theme"
+# #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
