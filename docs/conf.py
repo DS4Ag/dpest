14fac0c6c78bfbc37a7a22d120eac9fd48d8a977
luizvargaz <luizvargaz@hotmail.com>
2025-03-06 20:52:34 -0500

    Configuration files

--- docs/conf.py
+++ docs/conf.py
@@ -4,28 +4,21 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
-
 import sphinx.ext.viewcode
 
 sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'dpest'
 copyright = '2025, Luis Vargas-Rojas'
 author = 'Luis Vargas-Rojas'
-release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon','sphinx.ext.githubpages','sphinx.ext.autosummary']
-
-
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon','sphinx.ext.githubpages','sphinx.ext.autosummary', 'sphinx.ext.viewcode']
 autosummary_generate = True
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 autodoc_mock_imports = ["flopy", "pyemu"]
 
 # -- Options for HTML output -------------------------------------------------
@@ -33,10 +26,9 @@ autodoc_mock_imports = ["flopy", "pyemu"]
 
 html_theme = 'pydata_sphinx_theme'
 
-# html_theme = 'alabaster'
-# html_static_path = ['_static']
+html_show_sourcelink = True
 
-# import sphinx_rtd_theme
-# html_theme = "sphinx_rtd_theme"
-# #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
+html_theme_options = {
+"use_repository_button": True, 
+"use_source_button": True, 
+}
