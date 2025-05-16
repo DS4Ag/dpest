1b8381c9a91490e2aa2e11878d5f6043015db4ed
Luis Vargas Rojas <luizvargaz@hotmail.com>
2025-05-16 11:16:16 -0400

    Fixed documentation

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,6 @@ html_theme_options = {
     "use_source_button": True,
     "show_nav_level": 2,  # Ensure navigation is visible up to depth 2
     "navigation_with_keys": True,  # Enable keyboard navigation
-    "navbar_start": [],
     "navbar_center": [],
     "navbar_end": ["search-field.html"],  # <-- This puts the search box on the right
 }
